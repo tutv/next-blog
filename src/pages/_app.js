fe621277bb5a45e80becb3172ca5503a00a5478d
tutv <tutv@foobla.com>
2022-05-01 15:55:48 +0700

    added layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/index.scss"
+import Layout from "../layout/layout"
 
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
 
 export default MyApp
