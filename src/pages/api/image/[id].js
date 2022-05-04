1c0b8ca4d4e09b111b7740bbd5ad3e1e5de2b8ba
Tu TV <tutv@foobla.com>
2022-05-04 19:28:37 +0700

    updated

--- src/pages/api/image/[id].js
+++ src/pages/api/image/[id].js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
     try {
         const {id} = req.query
         const url = await getImageUrl(id)
-        console.log("URL:", url)
 
         res.redirect(url)
     } catch (error) {
