ec1485b46150d44f6376fed9cd7dd34c9b8626d5
Tu TV <tutv@foobla.com>
2022-05-05 01:06:02 +0700

    udpated

--- src/app/sidebar/ListCategories.js
+++ src/app/sidebar/ListCategories.js
@@ -1,38 +1,81 @@
 import {List} from "antd"
 import Link from "next/link"
+import {Component} from "react"
+import classNames from 'classnames'
+import dynamic from "next/dynamic"
 
 
-const data = [
-    'UNCATEGORIZED',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.'
-]
+const _fetchListCategories = async () => {
+    console.log("FETCHING...")
+    const res = await fetch('/api/categories/search')
+    const json = await res.json()
+    const {categories} = Object.assign({}, json)
+
+    return Array.isArray(categories) ? categories : []
+}
+
+class ListCategories extends Component {
+    state = {
+        categories: [],
+        loading: false
+    }
+
+    _fetchData = async () => {
+        if (this.state.loading) return
+
+        this.setState({
+            loading: true
+        })
+
+        console.log('fetching...')
+
+        try {
+            const categories = await _fetchListCategories()
+
+            this.setState({
+                categories,
+                loading: false
+            })
+
+        } catch (error) {
+            this.setState({
+                loading: false
+            })
+        }
 
 
-function ListCategories() {
-    return (
-        <div className="ListCategories">
-            <List
-                size="large"
-                header={<h3>Danh mục</h3>}
-                dataSource={data}
-                renderItem={(item, index) => {
-                    return (
-                        <List.Item key={index}>
-                            <Link href={`/category/${index + 1}`}>
-                                {item}
-                            </Link>
-                        </List.Item>
-                    )
-                }
-                }
-            />
-        </div>
-    )
+    }
+
+    componentDidMount() {
+        this._fetchData()
+    }
+
+    render() {
+        const {categories, loading} = this.state
+
+        return (
+            <div className={classNames("ListCategories", {loading})}>
+                <List
+                    size="large"
+                    header={<h3>Danh mục {loading ? 'loading...' : ''}</h3>}
+                    dataSource={categories}
+                    renderItem={(item, index) => {
+                        const {name, slug} = item
+
+                        return (
+                            <List.Item key={index}>
+                                <Link href={`/category/${slug}`}>
+                                    {name}
+                                </Link>
+                            </List.Item>
+                        )
+                    }
+                    }
+                />
+            </div>
+        )
+    }
 }
 
 
 export default ListCategories
-
