89397041c98416cd75f1015a0a5a095427c1200b
tutv <tutv@foobla.com>
2022-05-04 15:14:57 +0700

    added loading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import 'antd/dist/antd.css'
 import "../styles/index.scss"
 import Layout from "../layout/layout"
+import NextNProgress from "nextjs-progressbar"
 
 
 function MyApp({Component, pageProps}) {
 
     return (
         <Layout>
+            <NextNProgress/>
             <Component {...pageProps} />
         </Layout>
     )
